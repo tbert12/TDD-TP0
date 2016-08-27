1d7113eba2399d3fe4001ea656bc1a825bba1cda
tbert <tomasbert@hotmail.com>
2016-08-27 00:57:04 -0300

    Travis test

--- src/main/java/tp0/tbert12/cola/Main.java
+++ src/main/java/tp0/tbert12/cola/Main.java
@@ -2,7 +2,7 @@ package tp0.tbert12.cola;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I can run");
+        System.out.println("TP0 | 75.10 - TDD | 95606 - Tomas Augusto Bert");
     }
 }
 
